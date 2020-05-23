246d0c9c53d680d8afe058bd9413e427bca82cb0
Rishabh Kaushik <rishabhrkaushik@gmail.com>
2020-05-23 12:22:08 +0530

    added negotiation certificate

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.244bda6c7fe0bbc8656de0495ec4cdfa.js"
+  "/precache-manifest.6fc6032590eba29e4b1de62c9cecd26a.js"
 );
 
 self.addEventListener('message', (event) => {
